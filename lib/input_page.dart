4f508333566c1ac18f2b7d5fa3696585cb4b24ae
BL-CoderOvertime <akutenshi@live.com>
2019-12-06 21:57:03 -0500

    input_page separated, One of the 5 containers created.

---
+++ lib/input_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Color(0xFF1D1E33),
+        ),
+      ),
+    );
+  }
+}
