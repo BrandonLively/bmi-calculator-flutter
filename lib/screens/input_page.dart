39291d11c45f67efc9cea74b47fd4e08366d44f0
BL-CoderOvertime <akutenshi@live.com>
2019-12-08 15:35:11 -0500

    Project Complete routes are functional, arguments are passed and calculations are displayed properly.

--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -1,5 +1,7 @@
+import 'package:bmi_calculator/calculator_brain.dart';
 import 'package:bmi_calculator/components/icon_content.dart';
 import 'package:bmi_calculator/components/reusable_card.dart';
+import 'package:bmi_calculator/screens/results_page.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../components/bottom_button.dart';
@@ -227,7 +229,19 @@ class _InputPageState extends State<InputPage> {
           BottomButton(
             buttonTitle: 'CALCULATE',
             onTap: () {
-              Navigator.pushNamed(context, '/results');
+              CalculatorBrain calc =
+                  CalculatorBrain(height: height, weight: weight);
+
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ResultsPage(
+                    bmiResult: calc.calculateBMI(),
+                    resultText: calc.getResult(),
+                    interpretation: calc.getInterpretation(),
+                  ),
+                ),
+              );
             },
           ),
         ],
@@ -235,4 +249,3 @@ class _InputPageState extends State<InputPage> {
     );
   }
 }
-
